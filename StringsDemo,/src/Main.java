9bbc8b2c58514d528c40ff34817712586d217fd4
cuhadar8 <ibrahimcuhadar8@gmail.com>
2022-10-05 19:53:03 +0300

    StringsDemo

---
+++ StringsDemo,/src/Main.java
@@ -0,0 +1,39 @@
+
+public class Main {
+	public static void main(String[] args) {
+		
+		String mesaj = "Bugün hava çok güzel.";
+		
+		System.out.println(mesaj);
+		
+		/*System.out.println("Metnin karakter sayısı: " + mesaj.length());
+		System.out.println("Metnin 5. Elemeanı: " + mesaj.charAt(4));
+		System.out.println(mesaj.concat(" Yaşasın!"));
+		System.out.println(mesaj.startsWith("B"));
+		System.out.println(mesaj.endsWith("."));
+		
+		char[] karakterler = new char[5];
+		mesaj.getChars(0, 4, karakterler, 0);
+		
+		System.out.println(karakterler);
+		
+		System.out.println(mesaj.indexOf("a"));
+		System.out.println(mesaj.lastIndexOf("a"));*/
+		String yeniMesaj = mesaj.replace(' ', '*');
+		System.out.println(mesaj.replace(' ', '*'));
+		
+		System.out.println(mesaj.substring(2,4));
+		
+		System.out.println("******************");
+		
+		for(String kelime : mesaj.split(" ")) {
+			System.out.println(kelime);
+		}
+		
+		System.out.println(mesaj.toLowerCase());
+		System.out.println(mesaj.toUpperCase());
+		
+		System.out.println(mesaj.trim());
+	}
+
+}
